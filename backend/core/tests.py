497d1ca926e17f141b53bd2f90475023750412d4
James Gichia <jamesgichia15@gmail.com>
2026-07-08 09:22:46 +0300

    feat: login response includes org list with roles

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+from core.models import Organization, Membership
 
-# Create your tests here.
+User = get_user_model()
+
+
+class LoginResponseTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(
+            username='james', password='testpass123'
+        )
+        self.org = Organization.objects.create(name='Test Org')
+        Membership.objects.create(
+            user=self.user,
+            organization=self.org,
+            role=Membership.Role.ADMIN
+        )
+
+    def test_login_returns_org_list(self):
+        response = self.client.post('/api/v1/auth/token/', {
+            'username': 'james',
+            'password': 'testpass123'
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('organizations', response.data)
+        self.assertEqual(len(response.data['organizations']), 1)
+        self.assertEqual(response.data['organizations'][0]['name'], 'Test Org')
+        self.assertEqual(response.data['organizations'][0]['role'], 'admin')
